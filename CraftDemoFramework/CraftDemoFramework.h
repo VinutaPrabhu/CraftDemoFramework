b3e822ea91934eda7c997fecf1ceeaa76116861c
Vinuta Prabhu <vinuta.prabhu@sevenlakes.com>
2019-03-13 10:24:04 +0530

    Initial Commit

---
+++ CraftDemoFramework/CraftDemoFramework.h
@@ -0,0 +1,19 @@
+//
+//  CraftDemoFramework.h
+//  CraftDemoFramework
+//
+//  Created by Vinuta Prabhu on 13/03/19.
+//  Copyright © 2019 Seven Lakes Enterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CraftDemoFramework.
+FOUNDATION_EXPORT double CraftDemoFrameworkVersionNumber;
+
+//! Project version string for CraftDemoFramework.
+FOUNDATION_EXPORT const unsigned char CraftDemoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CraftDemoFramework/PublicHeader.h>
+
+
